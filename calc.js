bab587e2719d883c34bd59adf37126e606d2ab65
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2025-02-28 22:00:37 +0200

    backup

---
+++ calc.js
@@ -0,0 +1,95 @@
+// const x = 5;
+// const y = 5;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// let value = 5;
+// value = value + 10;
+// value += 10;
+// console.log(value)
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x < z);
+// console.log(x <= z);
+// console.log(x === y);
+// // console.log(x == z);
+// console.log(x === z);
+
+// const valueA = "5";
+// console.log(Number(valueA))
+// console.log(typeof Number(valueA));
+
+// const valueB = "Hello";
+// console.log(Number(valueB));
+// console.log(typeof Number(valueB));
+
+// console.log(Number.parseInt("5px"));
+// console.log(Number.parseInt("5rty"));
+// console.log(Number.parseInt("5.66"));
+// console.log(Number.parseInt("wefwef"));
+
+// console.log(Number.parseFloat("5px"));
+// console.log(Number.parseFloat("5.66"));
+
+// const validNumber = Number("51");
+// console.log(typeof validNumber);
+// console.log(Number.isNaN(validNumber));
+
+// const invalidNumber = Number("weuhif")
+// console.log(typeof invalidNumber);
+// console.log(Number.isNaN(invalidNumber));
+
+// let a = 0.5;
+// let b = 0.5;
+
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+// console.log(Math.floor(1.7));
+
+// console.log(Math.ceil(1.2));
+
+// console.log(Math.round(1.5));
+
+// console.log(Math.max(5, 10, 15, 20));
+
+// console.log(Math.min(5, 10, 15, 20));
+
+// console.log(Math.pow(2, 4)); 
+
+// let randomNumber = (Math.random() * (10 - 1) + 1);
+
+// console.log(randomNumber)
+
+
+// console.log(Math.random() * (10 - 1) + 1);
+
+// console.log(Math.floor(Math.random() * (10 - 1 + 1) + 1));
+// console.log(2 ** 4); 
+
+const x = Number(prompt("Введіть перше число для +:"));
+const y = Number(prompt("Введіть друге число для +:"));
+
+alert(x + " + " + y + " = " + (x + y));
+
+const z = Number(prompt("Введіть перше число для -:"));
+const v = Number(prompt("Введіть друге число для -:"));
+alert(z + " - " + v + " = " + (z - v));
+
+const a = Number(prompt("Введіть перше число для *:"));
+const b = Number(prompt("Введіть друге число для *:"));
+alert(a + " * " + b + " = " + (a * b));
+
+const n = Number(prompt("Введіть перше число для /:"));
+const m = Number(prompt("Введіть друге число для /:"));
+alert(n + " / " + m + " = " + (n / m));
+// alert(x + " + " + y + " = " + (x + y));
+// alert(x + " + " + y + " = " + (x + y));
